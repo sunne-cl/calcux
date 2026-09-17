ab14fd012b88ef2a350fa70bc3a286512f01f5be
Vexikoff <vexikoff@gmail.com>
2026-09-17 15:30:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -29,24 +29,6 @@ downButton.addEventListener('mouseleave', function() {
     downButton.style.transform = 'rotateX(0deg) rotateY(0deg) translate3d(0px, 0px, 0px)';
 });
 
-const downButton1 = document.getElementById('downbutton1');
-
-downButton1.addEventListener('mousemove', function(event) {
-    const rect = downButton1.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-    const percentX = (mouseX - centerX) / centerX;
-    const percentY = (mouseY - centerY) / centerY;
-    const rotateX = percentY * maxRotationX;
-    const rotateY = percentX * maxRotationY;
-    const translateX = percentX * maxTranslation;
-    const translateY = percentY * maxTranslation;
-    
-    downButton1.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translate3d(${translateX}px, ${translateY}px, 0px)`;
-});
-
 downButton1.addEventListener('mouseleave', function() {
     downButton1.style.transform = 'rotateX(0deg) rotateY(0deg) translate3d(0px, 0px, 0px)';
 });
