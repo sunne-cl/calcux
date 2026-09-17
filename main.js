817a329caf36e37bdf89c0aef74020021fd9aa46
Vexikoff <vexikoff@gmail.com>
2026-09-17 12:10:37 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,92 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const fs = require('fs');
+// BY VEXIKOFF
+// BY VEXIKOFF
+// BY VEXIKOFF
+// BY VEXIKOFF
+// BY VEXIKOFF
 
-let mainWindow;
+const downButton = document.getElementById('downbutton');
+const maxRotationX = -2.5;
+const maxRotationY = -0.5;
+const maxTranslation = 3;
 
-const gotTheLock = app.requestSingleInstanceLock();
+downButton.addEventListener('mousemove', function(event) {
+    const rect = downButton.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const percentX = (mouseX - centerX) / centerX;
+    const percentY = (mouseY - centerY) / centerY;
+    const rotateX = percentY * maxRotationX;
+    const rotateY = percentX * maxRotationY;
+    const translateX = percentX * maxTranslation;
+    const translateY = percentY * maxTranslation;
+    
+    downButton.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translate3d(${translateX}px, ${translateY}px, 0px)`;
+});
 
-if (!gotTheLock) {
-  app.quit();
-} else {
+downButton.addEventListener('mouseleave', function() {
+    downButton.style.transform = 'rotateX(0deg) rotateY(0deg) translate3d(0px, 0px, 0px)';
+});
 
-  app.whenReady().then(createWindow);
+const screenshot = document.getElementById('screenshot');
+
+const maxRotationXc = -0.1;
+const maxRotationYc = -2.5;
+const maxTranslationc = 0.5;
+
+screenshot.addEventListener('mousemove', function(event) {
+    const rect = downButton.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const percentX = (mouseX - centerX) / centerX;
+    const percentY = (mouseY - centerY) / centerY;
+    const rotateX = percentY * maxRotationXc;
+    const rotateY = percentX * maxRotationYc;
+    const translateX = percentX * maxTranslationc;
+    const translateY = percentY * maxTranslationc;
+
+    screenshot.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translate3d(${translateX}px, ${translateY}px, 0px)`;
+});
+
+screenshot.addEventListener('mouseleave', function() {
+    screenshot.style.transform = 'rotateX(0deg) rotateY(0deg) translate3d(0px, 0px, 0px)';
+});
+
+const screenshotw = document.getElementById('screenshotw');
+
+screenshotw.addEventListener('mousemove', function(event) {
+    const rect = downButton.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const percentX = (mouseX - centerX) / centerX;
+    const percentY = (mouseY - centerY) / centerY;
+    const rotateX = percentY * maxRotationXc;
+    const rotateY = percentX * maxRotationYc;
+    const translateX = percentX * maxTranslationc;
+    const translateY = percentY * maxTranslationc;
+
+    screenshotw.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translate3d(${translateX}px, ${translateY}px, 0px)`;
+});
+
+screenshot.addEventListener('mouseleave', function() {
+    screenshotw.style.transform = 'rotateX(0deg) rotateY(0deg) translate3d(0px, 0px, 0px)';
+});
+
+function towhite() {
+    screenshotw.classList.remove("hidden")
+    screenshot.classList.add("hidden")
 }
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 300,
-    height: 450,
-    frame: false,
-    transparent: true,
-    resizable: false,
-    maximizable: false,
-    icon: path.join(__dirname, 'assets/logos/logo.ico'),
-    backgroundColor: '#00000000',
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
-  
-  mainWindow.loadFile('index.html');
+function todark() {
+    screenshotw.classList.add("hidden")
+    screenshot.classList.remove("hidden")
+}
 
-  ipcMain.on('window-controls', (event, action) => {
-    if (action === 'close') mainWindow.close();
-    if (action === 'minimize') mainWindow.minimize();
-    if (action === 'toggle-size') {
-      const [width, height] = mainWindow.getSize();
-      mainWindow.setResizable(true);
-      if (width === 300) {
-        mainWindow.setSize(600, 450);
-      } else {
-        setTimeout(() => {
-          mainWindow.setSize(300, 450);
-        }, 300);
-      }
-    }
-  });
-
+function goto(url) {
+    window.open(url, '_blank');
 }
